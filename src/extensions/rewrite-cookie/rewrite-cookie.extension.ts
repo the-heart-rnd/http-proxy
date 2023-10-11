69f3da72b4ed09e5f1beff4f363ca7fe91e5b9ca
Piotr Bosak <hitori@duck.com>
2023-10-11 12:26:30 +0200

    fix(rewrite-cookie): fix subdomain rewriting (#18)

--- src/extensions/rewrite-cookie/rewrite-cookie.extension.ts
+++ src/extensions/rewrite-cookie/rewrite-cookie.extension.ts
@@ -29,10 +29,53 @@ export class RewriteCookieExtension extends ProxyExtension {
         'Rewriting cookie from: ' + targetHost + ' to: ' + proxyHost,
       );
       context.serviceResponseHeaders.update('set-cookie', (cookie) =>
-        cookie.replaceAll(`Domain=${targetHost}`, `Domain=${proxyHost}`),
+        this.rewriteCookie(cookie, targetHost, proxyHost),
       );
     }
 
     return context;
   };
+
+  private rewriteCookie(
+    cookie: string,
+    requestedHost: string,
+    rewriteHost: string,
+  ): string {
+    // get domain from cookie
+    const [toReplace, cookieDomain] = cookie.match(/Domain=([^;]+)/) ?? [];
+    if (!toReplace || !cookieDomain) {
+      return cookie;
+    }
+
+    if (!this.domainMatches(cookieDomain, requestedHost)) {
+      return cookie;
+    }
+
+    return cookie.replaceAll(toReplace, `Domain=${rewriteHost}`);
+  }
+
+  private domainMatches(domain: string, host: string): boolean {
+    // .sth.com matches sth.com
+    // sth.com matches sth.com
+    // sth.com does not match nonsth.com
+    // nonsth.com does not match sth.com
+    // a.sth.com matches sth.com
+    // sth.com matches a.sth.com
+    // a.sth.com matches .sth.com
+    // a.sth.com does not match b.sth.com
+
+    const domainGtHost = domain.length;
+    const longer = domainGtHost > host.length ? domain : host;
+    const shorter = domainGtHost > host.length ? host : domain;
+
+    if (longer === shorter) {
+      return true;
+    }
+
+    if (shorter[0] !== '.') {
+      return longer.endsWith(`.${shorter}`);
+    }
+
+    return longer.endsWith(shorter);
+  }
 }
